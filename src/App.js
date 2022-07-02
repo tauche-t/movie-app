8143558cf5ad9fc568f7f7a26af201fc7a7d3e8e
tauche-t <tauche.t3473@gmail.com>
2022-07-02 18:56:50 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/movie/:id" element={<Detail />}>
+        </Route>
+        <Route path="/" element={<Home />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
